57d48e712f9cc6b489eb0b322278944e2e8d7c23
KayumovSher <shermuhammad.qayumov.2002@gmail.com>
2024-10-29 10:19:44 +0500

    images created

--- reactapp/src/components/Body.js
+++ reactapp/src/components/Body.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import './Body.css'
 
 
+
 export default function Body() {
+    const [isLoading, setIsloading] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [data, setData] = useState([]);
+
+    const fetchProducts = async() =>{
+        try {
+            setIsloading(true);
+            setIsError(false);
+
+            const resPro = await fetch(
+                `https://dummyjson.com/products?limit=20&skip=10`
+            );
+
+            const res = await resPro.json()
+
+            console.log(res);
+
+        } catch (err) {
+            setIsError(true);
+        } finally {
+            setIsloading(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
     return (
-        <div className='hero'>
+        <div>
             <h1>Hello World</h1>
         </div>
     );
